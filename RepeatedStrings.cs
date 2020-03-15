998ef0d4bae6cde6cd8a1ed592fde71d2ff2a082
Prashankumar Thesencherri Shanmugam <60387632+Prashankumar-Tech@users.noreply.github.com>
2020-03-15 14:32:02 +0000

    initial commit

---
+++ RepeatedStrings.cs
@@ -0,0 +1,58 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the repeatedString function below.
+    static long repeatedString(string s, long n)
+    {
+        int stringLength = s.Length;
+        long count = 0;
+        long div = n / stringLength;
+        long reminder = n % stringLength;
+
+        foreach (char charString in s.ToCharArray())
+        {
+            if (charString == 'a')
+                count++;
+        }
+
+        count = div * count;
+        for (int i = 0; i < reminder; i++)
+        {
+            if (s[i] == 'a')
+                count++;
+        }
+
+        return count;
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        long n = Convert.ToInt64(Console.ReadLine());
+
+        long result = repeatedString(s, n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
